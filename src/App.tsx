52810c0cf9183cd1b9a120414bda8b5dc79bad12
jakub-ner <kubaner1@gmail.com>
2023-09-21 13:05:17 +0200

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { NativeRouter, Route, Routes } from "react-router-native";
-import MenuScreen from "./screens/MenuScreen";
+import {NativeRouter, Route, Routes} from 'react-router-native';
+import MenuScreen from './screens/MenuScreen';
 
 export default function App() {
   return (
     <NativeRouter>
       <Routes>
-        <Route path="/" element={<MenuScreen/>}/>
+        <Route path="/" element={<MenuScreen />} />
       </Routes>
     </NativeRouter>
   );
 }
-
--- src/screens/MenuScreen.tsx
+++ src/screens/MenuScreen.tsx
@@ -1,75 +1,81 @@
-import {ImageBackground, View, Text, StyleSheet, TouchableHighlight} from "react-native";
+import {
+  ImageBackground,
+  View,
+  Text,
+  StyleSheet,
+  TouchableHighlight,
+} from 'react-native';
 
-const image = {uri: 'https://wallpapers.com/images/high/pink-hearts-background-2nmnoa3disyli8si.webp'};
-
-export default function MenuScreen() {
-    return (
-        <ImageBackground source={image} resizeMode="cover" style={stylesImg.image}>
-
-            <View style={styles.container}>
-                <Text style={styles.text}>Hejeczka :***xoxo</Text>
-                <TouchableHighlight
-                    style={styles.button} onPress={() => console.log("Pressed")}>
-                    <View style={styles.button}>
-                        <Text style={styles.buttonText}>NOTATECZKI</Text>
-                    </View>
-                </TouchableHighlight>
-
-                <TouchableHighlight
-                    style={styles.button} onPress={() => console.log("Pressed")}>
-                    <View style={styles.button}>
-                        <Text style={styles.buttonText}>PLOTECZKI</Text>
-                    </View>
-                </TouchableHighlight>
-
-                <TouchableHighlight
-                    style={styles.button} onPress={() => console.log("Pressed")}>
-                    <View style={styles.button}>
-                        <Text style={styles.buttonText}>MEMY</Text>
-                    </View>
-                </TouchableHighlight>
-
-            </View>
-
-        </ImageBackground>
-
-    );
+const image = {
+  uri: 'https://wallpapers.com/images/high/pink-hearts-background-2nmnoa3disyli8si.webp',
 };
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+export default function MenuScreen() {
+  return (
+    <ImageBackground source={image} resizeMode="cover" style={stylesImg.image}>
+      <View style={styles.container}>
+        <Text style={styles.text}>Hejeczka :***xoxo</Text>
+        <TouchableHighlight
+          style={styles.button}
+          onPress={() => console.log('Pressed')}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>NOTATECZKI</Text>
+          </View>
+        </TouchableHighlight>
 
-    },
-    text: {
-        color: 'black',
-        fontSize: 35,
-        lineHeight: 84,
-        textAlign: 'center'
-    },
-    button: {
-        height: 100,
-        width: 200,
-        backgroundColor: "#808080c0",
-        justifyContent: "center", // Wyśrodkowanie tekstu w pionie
-        alignItems: "center",     // Wyśrodkowanie tekstu w poziomie
-        borderRadius: 10,         // Zaokrąglenie rogów przycisku
-        marginVertical: 10,
-    },
-    buttonText: {
-        color: "pink",
-        fontSize: 30,
-    },
+        <TouchableHighlight
+          style={styles.button}
+          onPress={() => console.log('Pressed')}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>PLOTECZKI</Text>
+          </View>
+        </TouchableHighlight>
+
+        <TouchableHighlight
+          style={styles.button}
+          onPress={() => console.log('Pressed')}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>MEMY</Text>
+          </View>
+        </TouchableHighlight>
+      </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'black',
+    fontSize: 35,
+    lineHeight: 84,
+    textAlign: 'center',
+  },
+  button: {
+    height: 100,
+    width: 200,
+    backgroundColor: '#808080c0',
+    justifyContent: 'center', // Wyśrodkowanie tekstu w pionie
+    alignItems: 'center', // Wyśrodkowanie tekstu w poziomie
+    borderRadius: 10, // Zaokrąglenie rogów przycisku
+    marginVertical: 10,
+  },
+  buttonText: {
+    color: 'pink',
+    fontSize: 30,
+  },
 });
 
 const stylesImg = StyleSheet.create({
-    container: {
-        flex: 1
-    },
-    image: {
-        flex: 1,
-        justifyContent: 'center',
-    }
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
 });
